24bce1c4d67a7c7c6f6fb0370b33b4c2febed1b6
GasparRios <gasparrios1234@gmail.com>
2026-06-30 15:31:31 -0300

    Hero: usar RenderChat.png como fondo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,7 @@ export default function Hero() {
       {/* Background Image */}
 <div className="absolute inset-0 z-0">
   <img
-    src="https://raw.githubusercontent.com/GasparRios/nvm-assets/2f52cad64e80664a0da76b1797fc24219550cf51/Render3White.png" 
+    src="https://raw.githubusercontent.com/GasparRios/nvm-assets/92bad593cd5e3a2770bde9bdab647f80fe95fd07/RenderChat.png" 
     alt="Carbon fiber"
     className="w-full h-full object-cover"
   />
